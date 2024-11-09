380f24362cc67cf84cd6351d3e5c276bf6e7727c
luffy977338 <timur.djeletyan.2009@gmail.com>
2024-11-09 11:21:55 +0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,8 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      colors: {},
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
