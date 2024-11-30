3fd97cfb383909f979ef59183cf77a8c0acfbfab
luffy977338 <timur.djeletyan.2009@gmail.com>
2024-11-30 21:03:59 +0300

    prod

--- auth.js
+++ auth.js
@@ -7,6 +7,21 @@ import prisma from "./lib/prisma";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   trustHost: true,
   adapter: PrismaAdapter(prisma),
+  cookies: {
+    sessionToken: {
+      name: `next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        secure: process.env.NODE_ENV === "production",
+      },
+    },
+  },
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      return url.startsWith(baseUrl) ? url : baseUrl;
+    },
+  },
   providers: [
     Google,
     Email({
